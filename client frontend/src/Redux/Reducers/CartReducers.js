de181e3db7c1b025dbcce3d3ff5b6752b69ae780
Khoi Le <90933354+krissaint@users.noreply.github.com>
2024-05-12 15:13:40 -0700

    Update CartReducers.js

--- client frontend/src/Redux/Reducers/CartReducers.js
+++ client frontend/src/Redux/Reducers/CartReducers.js
@@ -4,7 +4,7 @@ import {
   CART_REMOVE_ITEM,
   CART_SAVE_PAYMENT_METHOD,
   CART_SAVE_SHIPPING_ADDRESS,
-} from "../Constants/CartConstants";
+} from "../Constants/CartConstants"; 
 
 export const cartReducer = (
   state = { cartItems: [], shippingAddress: {} },
