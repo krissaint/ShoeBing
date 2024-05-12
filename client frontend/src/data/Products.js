9d16df86f203efb21bba0e0f548824c14b55f740
Khoi Le <90933354+krissaint@users.noreply.github.com>
2024-05-12 15:08:53 -0700

    Update Products.js

--- client frontend/src/data/Products.js
+++ client frontend/src/data/Products.js
@@ -7,7 +7,7 @@ const products = [
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
     price: 89,
     countInStock: 3,
-    rating: 4,
+    rating: 4, 
     numReviews: 4,
   },
   {
