9526278faeb9a2b6914cc29b64676249cd7fcde2
Khoi Le <90933354+krissaint@users.noreply.github.com>
2024-05-12 15:15:46 -0700

    Update PrivateRouter.js

--- client frontend/src/PrivateRouter.js
+++ client frontend/src/PrivateRouter.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Redirect, Route } from "react-router-dom";
+import { Redirect, Route } from "react-router-dom"; 
 
 function PrivateRouter({ component: Component, ...rest }) {
   return (
