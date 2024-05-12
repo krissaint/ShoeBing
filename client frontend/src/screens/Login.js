df2c5d70b997ea04685849c0eb07da5a22bb43ef
Khoi Le <90933354+krissaint@users.noreply.github.com>
2024-05-12 15:05:23 -0700

    Update Login.js

--- client frontend/src/screens/Login.js
+++ client frontend/src/screens/Login.js
@@ -5,7 +5,7 @@ import Message from "../components/LoadingError/Error";
 import Loading from "../components/LoadingError/Loading";
 import Header from "./../components/Header";
 import { login } from "./../Redux/Actions/userActions";
-
+ 
 const Login = ({ location, history }) => {
   window.scrollTo(0, 0);
   const [email, setEmail] = useState("");
