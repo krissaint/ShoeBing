bdb1969717ca8c0bb816a999a8e7d68be6954faf
Khoi Le <90933354+krissaint@users.noreply.github.com>
2024-05-12 15:05:10 -0700

    Update HomeScreen.js

--- client frontend/src/screens/HomeScreen.js
+++ client frontend/src/screens/HomeScreen.js
@@ -3,7 +3,7 @@ import Header from "./../components/Header";
 import ShopSection from "./../components/homeComponents/ShopSection";
 import ContactInfo from "./../components/homeComponents/ContactInfo";
 import CalltoActionSection from "./../components/homeComponents/CalltoActionSection";
-import Footer from "./../components/Footer";
+import Footer from "./../components/Footer"; 
 
 const HomeScreen = ({ match }) => {
   window.scrollTo(0, 0);
